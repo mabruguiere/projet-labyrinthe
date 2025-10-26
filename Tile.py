591c0571cba1d747bedfaec116f520c071e96b82
Mathys <mathys.bruguiere@gmail.com>
2025-10-26 13:36:18 +0100

    le programme affiche désormais la grille qui va nous servir de labyrinthe

---
+++ Tile.py
@@ -0,0 +1,29 @@
+TILE_SIZE = 25
+
+class Tile:
+    def __init__(self,x,y, canvas):
+        self.x = x
+        self.y = y
+        self.visited = False
+        self.neighbours = []
+
+        #wall position to draw them
+        self.topLeftCornerX = self.x
+        self.topLeftCornerY = self.y
+        self.bottomLeftCornerX = self.x 
+        self.bottomLeftCornerY = self.y + TILE_SIZE
+        self.topRightCornerX = self.x + TILE_SIZE
+        self.topRightCornerY = self.y
+        self.bottomRightCornerX = self.x + TILE_SIZE
+        self.bottomRightCornerY = self.y + TILE_SIZE
+
+
+        #coordinates of the walls 
+
+        self.draw_walls(canvas)
+
+    def draw_walls(self,canvas):
+        self.topWall = canvas.create_line(self.topLeftCornerX,self.topLeftCornerY, self.topRightCornerX, self.topRightCornerY, width=2, fill="black")
+        self.leftWall = canvas.create_line(self.topLeftCornerX,self.topLeftCornerY, self.bottomLeftCornerX, self.bottomLeftCornerY, width=2, fill="black")
+        self.bottomWall = canvas.create_line(self.bottomLeftCornerX, self.bottomLeftCornerY, self.bottomRightCornerX, self.bottomRightCornerY, width=2, fill="black")
+        self.rightWall = canvas.create_line(self.bottomRightCornerX, self.bottomRightCornerY, self.topRightCornerX, self.topRightCornerY, width=2, fill="black")
