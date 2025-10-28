303e74ca2e075e2e631d0322c8ffd316e3339b4a
Mathys <mathys.bruguiere@gmail.com>
2025-10-28 09:37:58 +0100

    création de bouton pour décider quel algorithme lancer

--- Main.py
+++ Main.py
@@ -9,31 +9,26 @@ COLS = 25
 TILE_SIZE = 25 #25 pixels
 
 WINDOW_WIDTH = COLS * TILE_SIZE
-WINDOWS_HEIGHT = ROWS * TILE_SIZE
+WINDOW_HEIGHT = ROWS * TILE_SIZE
 
 #maze window
 windows = tk.Tk()
 windows.title("Labyrinthe")
 windows.resizable(False,False) 
 
-canvas = tk.Canvas(windows, width=WINDOW_WIDTH, height=WINDOWS_HEIGHT, borderwidth=0, highlightthickness=0, background="white")
-canvas.pack()
-windows.update()
+main_frame = tk.Frame(windows)
+main_frame.pack(padx=10, pady=10)
 
-#We center the window
-window_width = windows.winfo_width() 
-window_height = windows.winfo_height()
-screen_width = windows.winfo_screenwidth()
-screen_height = windows.winfo_screenheight()
+buttons_frame = tk.Frame(main_frame)
+buttons_frame.grid(row=0, column=0, padx=10, sticky="n")
 
-windows_x = int((screen_width/2)-(window_width/2))
-windows_y = int((screen_height/2)-(window_height/2))
-
-windows.geometry(f"{window_height}x{window_width}+{windows_x}+{windows_y}") #geometry expect a string not an int, this places the window to the center of the screen
+canvas = tk.Canvas(main_frame, width=WINDOW_WIDTH, height=WINDOW_HEIGHT, background="white")
+canvas.grid(row=0, column=1)
 
 maze = Maze.Maze(canvas)
-#maze.recursive_dfs(canvas)
-#maze.recursive_division(canvas, 0 * TILE_SIZE, ROWS * TILE_SIZE, 0 * TILE_SIZE , COLS * TILE_SIZE)
-maze.prims_algorithm(canvas)
+
+tk.Button(buttons_frame, text="Prim", command=lambda: maze.prims_algorithm(canvas)).pack(side="top", pady=50)
+tk.Button(buttons_frame, text="Division", command=lambda: maze.recursive_division(canvas, 0, ROWS*TILE_SIZE, 0, COLS*TILE_SIZE)).pack(side="top", pady=50)
+tk.Button(buttons_frame, text="DFS", command=lambda: maze.recursive_dfs(canvas)).pack(side="top", pady=50)
 
 windows.mainloop()
